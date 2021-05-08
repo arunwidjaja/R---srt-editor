060452440141ca862dfe70fb9e54cd362df1cdc5
arunwidjaja <59853579+arunwidjaja@users.noreply.github.com>
2021-05-07 23:54:25 -0700

    Add files via upload

--- subshift.R
+++ subshift.R
@@ -5,7 +5,7 @@
 # an .srt subtitle file that has ".en" in the name.
 
 # Output:
-# an .srt subtitle file, edited according to the constants.
+# an .srt subtitle file, shifted according to the constants - see CONSTANTS section
 
 #### LIBRARIES ####
 
@@ -18,7 +18,7 @@ SHIFT_AMOUNT=-12000
 
 #### READING FILE ####
 
-setwd("C:/Users/Arun Widjaja/Documents/R Scripts")
+setwd("C:/Users/Arun Widjaja/Documents/R Scripts/srt editor")
 fn_sub=dir(pattern="\\.en\\.srt")
 
 print(paste("READING:",fn_sub))
@@ -26,17 +26,22 @@ sub_raw=readLines(fn_sub)
 
 #### PARSING FILE ####
 
-# extract times
+# read .srt file as a 1-column data frame
 sub_parsed=data.frame(sub_raw,stringsAsFactors=F)
 names(sub_parsed)="sub"
 
+# add column containing starting timestamps of each subtitle
 sub_parsed$t1=str_extract(sub_parsed$sub,"\\d{2}:\\d{2}:\\d{2},\\d{3}")
 
+# add column containing ending timestamps of each subtitle
 sub_parsed$t2=str_extract(sub_parsed$sub,"-->.*\\d{2}:\\d{2}:\\d{2},\\d{3}")
 sub_parsed$t2=str_replace(sub_parsed$t2,"-->\\s{1,}","")
+
+# replace commas with periods to fit timestamp format
 sub_parsed$t1=str_replace(sub_parsed$t1,",",".")
 sub_parsed$t2=str_replace(sub_parsed$t2,",",".")
 
+# get indices of rows containing timestamps
 index_times=which(!is.na(sub_parsed$t1))
 
 # split times into h, m, s, ms
